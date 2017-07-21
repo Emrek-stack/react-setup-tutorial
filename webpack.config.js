175e877ba1fd909fdc58808ed9c6b8fa479564a4
Emre Karahan <emre.karahan@dogangazetecilik.com.tr>
2017-07-21 14:38:52 +0300

    ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,5 @@
 const path = require('path');
-//var webpack = require('webpack');
-
-
 var VENDOR_PATH = __dirname + '/node_modules';
-
-var PROD = 1;
 
 var config = {
     entry: path.resolve(__dirname, './src/index.js'),
@@ -44,17 +39,5 @@ var config = {
         ],
     },
 }
-
-// config.addExternal('jquery', '$');
-// config.addExternal('react', 'React');
-// config.addExternal('react-dom', 'ReactDOM');
-
-// config.addVendor('react-router', VENDOR_PATH + '/react-router/umd/ReactRouter.js')
-// config.addVendor('react-bootstrap', VENDOR_PATH + '/react-bootstrap/dist/react-bootstrap.js');
-//config.addVendor('react-router-bootstrap', VENDOR_PATH + '/react-router-bootstrap/lib/index.js');
-
-//config.addPlugin(new CommonsChunkPlugin('vendors', 'vendors.js'));
-//config.addPlugin(new CommonsChunkPlugin({ name: 'vendors', filename: 'vendors.bundle.js' }));
-
 
 module.exports = config;
